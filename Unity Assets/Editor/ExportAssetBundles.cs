1715b6d72e06217c8ee97084b7cb028af291aaeb
Amy Nagle <kabi-git@openmuffin.com>
2020-12-03 01:17:48 -0500

    Add Unity assets

---
+++ Unity Assets/Editor/ExportAssetBundles.cs
@@ -0,0 +1,12 @@
+﻿// Create an AssetBundle for Windows.
+using UnityEngine;
+using UnityEditor;
+
+public class ExportAssetBundles : MonoBehaviour
+{
+    [UnityEditor.MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles("AssetBundles", BuildAssetBundleOptions.StrictMode, BuildTarget.StandaloneWindows);
+    }
+}
