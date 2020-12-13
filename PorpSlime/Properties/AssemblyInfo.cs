9898dcef23c8dfa9d96998069e8578cc7cd1a2bd
Amy Nagle <kabi-git@openmuffin.com>
2020-12-13 00:39:11 -0500

    Update version to 2.2

--- PorpSlime/Properties/AssemblyInfo.cs
+++ PorpSlime/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Security.Permissions;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("2.2")]
 
